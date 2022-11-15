1cde068d415aa6edeedf2b614f23d12ddda0d4da
Matías Roy Rodriguez <matiasrr980@gmail.com>
2022-11-15 16:59:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { increment, decrement, incrementByNumber } from './store/slices/counter/counterSlice';
+
+function App() {
+
+
+	const { counter } = useSelector(state => state.counter)
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://reactjs.org" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>	count is {counter}</h1>
+			<div className="card">
+				<button onClick={() => dispatch(increment())}>
+					Increment
+				</button>
+				<button onClick={() => dispatch(decrement())}>
+					Decrement
+				</button>
+				<button onClick={() => dispatch(incrementByNumber(2))}>
+					Increment by 2
+				</button>
+			</div>
+
+		</div>
+	)
+}
+
+export default App
---
+++ src/TodoApp.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis/todosApi"
+
+export const TodoApp = () => {
+
+	const [todoId, setTodoId] = useState(1)
+
+	// const { data: todos = [], isLoading } = useGetTodosQuery();
+	const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+	const nextTodo = () => {
+		setTodoId(todoId + 1);
+	}
+	const prevTodo = () => {
+		if (todoId === 1) return;
+		setTodoId(todoId - 1);
+	}
+
+	return (
+		<>
+			<h1>Todos - RTX Query</h1>
+			<hr />
+			<h4>isLoading: {isLoading ? 'true' : 'false'}</h4>
+			<pre >{JSON.stringify(todo)}</pre>
+			{/* <ul>
+				{todos.map(todo => (
+					<li key={todo.id}>
+						<strong> {todo.completed ? 'DONE' : 'PENDING'}</strong>	{todo.title}</li>
+				))}
+			</ul> */}
+			<button onClick={prevTodo}>Prev TODO</button>
+			<button onClick={nextTodo}>Next TODO</button>
+		</>
+	)
+}
+
